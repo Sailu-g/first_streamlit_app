abce0d9be79974eae48d4fa2fd95b3a5d5775927
Sailu-g <80976218+Sailu-g@users.noreply.github.com>
2024-03-13 15:47:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('breakfast menu')
 streamlit.text('Omega oatmeal & blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
